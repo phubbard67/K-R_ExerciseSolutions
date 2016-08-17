622996f58f2df7492a3515071134ab983a7e6bfa
Edmond Dantes <phubbard67@gmail.com>
2016-08-17 16:00:09 -0700

    exercise 1-3

---
+++ ex1.3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/* print Fahrenheit-Celsius table
+	for fahr = 0, 20, ..., 300; floating-point version */
+
+int main(int argc, char *argv[]){
+	
+	float fahr, celsius;
+	int lower, upper, step;
+
+	lower = 0;
+	upper = 300;
+	step = 20;
+
+	fahr = lower;
+
+	printf("\n");
+	printf("Fahrenheit  Celsius\n");
+	printf("--------------------\n");
+	while (fahr <= upper){
+		celsius = (5.0/9.0) * (fahr-32.0);
+		printf("  %3.0f      %6.1f\n", fahr, celsius);
+		fahr = fahr + step;
+	}
+	
+	return -1;
+}
